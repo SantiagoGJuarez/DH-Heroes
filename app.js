aef29bedd1a964e26d8c1d4a17c1e5ba9d509604
Santiago Juarez <santiagogjuarez@gmail.com>
2021-06-28 22:32:52 -0300

    Primer commit

---
+++ app.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.use(express.static('DH-Heroes'))
+
+app.listen(3030, () => {
+    console.log('Servidor corriendo');
+})
+
+app.get('/home', (req, res) => {
+    res.sendFile(path.join(__dirname, '/DH-Heroes/views/index.html'))
+})
+
+app.get('/babbage', (req, res) => {
+    res.sendFile(path.join(__dirname, '/DH-Heroes/views/babbage.html'))
+})
+
+app.get('/berners-lee', (req, res) => {
+    res.sendFile(path.join(__dirname, '/DH-Heroes/views/berners-lee.html'))
+})
+
+app.get('/clarke', (req, res) => {
+    res.sendFile(path.join(__dirname, '/DH-Heroes/views/clarke.html'))
+})
+
+app.get('/hamilton', (req, res) => {
+    res.sendFile(path.join(__dirname, '/DH-Heroes/views/hamilton.html'))
+})
+
+app.get('/hopper', (req, res) => {
+    res.sendFile(path.join(__dirname, '/DH-Heroes/views/hopper.html'))
+})
+
+app.get('/lovelace', (req, res) => {
+    res.sendFile(path.join(__dirname, '/DH-Heroes/views/lovelace.html'))
+})
+
+app.get('/turing', (req, res) => {
+    res.sendFile(path.join(__dirname, '/DH-Heroes/views/turing.html'))
+})
